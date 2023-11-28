4da8e4ced9d6c5965fe78d6f6dc1d1c8e7ffba08
spatulatom <spatulatom@gmail.com>
2023-11-28 12:43:46 +0000

    11. Container With Most Water, brute force, O(n^2)

--- src/app.ts
+++ src/app.ts
@@ -325,6 +325,49 @@ function canJump(nums: number[]): boolean {
 }
 console.log('canJump', canJump([0]));
 
+// 11. Container With Most Water
+// Medium
+
+// You are given an integer array height of length n. There are n vertical lines drawn such
+// that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+// Find two lines that together with the x-axis form a container, such that the container
+// contains the most water.
+
+// Return the maximum amount of water a container can store.
+
+// Notice that you may not slant the container.
+
+// Example 1:
+
+// Input: height = [1,8,6,2,5,4,8,3,7]
+// Output: 49
+// Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7].
+// In this case, the max area of water (blue section) the container can contain is 49.
+
+// Example 2:
+// Input: height = [1,1]
+// Output: 1
+
+// Constraints:
+// n == height.length
+// 2 <= n <= 105
+// 0 <= height[i] <= 104
+
+// sol 1, brute force approach
+// for every element x and y check this:
+// smaller el (x or y) * distance between elements = area with water
+function maxArea1(height: number[]): number {
+  let maxArea = 0;
+  for (let i = 0; i < height.length - 1; i++) {
+    for (let j = 1; j < height.length; j++) {
+      const area = Math.min(height[i], height[j]) * (j - i);
+      maxArea = Math.max(maxArea, area);
+    }
+  }
+
+  return maxArea;
+}
 
 
-
+console.log('maxArea', maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
