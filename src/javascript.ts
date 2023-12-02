a29f9a629d2578cca5feb9f74b1d6ac015c2bf84
spatulatom <spatulatom@gmail.com>
2023-12-02 18:47:08 +0000

    2695. Array Wrapper, sol

--- src/javascript.ts
+++ src/javascript.ts
@@ -316,3 +316,102 @@ console.log(counter.increment());
  * counter.reset(); // 5
  * counter.decrement(); // 4
  */
+
+
+// 2695. Array Wrapper
+// Easy
+// 196
+// 29
+// Companies
+// Create a class ArrayWrapper that accepts an array of integers in its constructor. 
+// This class should have two features:
+
+// When two instances of this class are added together with the + operator, the resulting 
+// value is the sum of all the elements in both arrays.
+// When the String() function is called on the instance, it will return a comma separated 
+// string surrounded by brackets. For example, [1,2,3].
+ 
+
+// Example 1:
+
+// Input: nums = [[1,2],[3,4]], operation = "Add"
+// Output: 10
+// Explanation:
+// const obj1 = new ArrayWrapper([1,2]);
+// const obj2 = new ArrayWrapper([3,4]);
+// obj1 + obj2; // 10
+// Example 2:
+
+// Input: nums = [[23,98,42,70]], operation = "String"
+// Output: "[23,98,42,70]"
+// Explanation:
+// const obj = new ArrayWrapper([23,98,42,70]);
+// String(obj); // "[23,98,42,70]"
+// Example 3:
+
+// Input: nums = [[],[]], operation = "Add"
+// Output: 0
+// Explanation:
+// const obj1 = new ArrayWrapper([]);
+// const obj2 = new ArrayWrapper([]);
+// obj1 + obj2; // 0
+ 
+
+// Constraints:
+
+// 0 <= nums.length <= 1000
+// 0 <= nums[i] <= 1000
+// Note: nums is the array passed to the constructor
+
+
+// sol 1
+// we are overriding default methods exosting on every object
+// - he valueOf method is called automatically when an object is used in a context
+//  where a primitive value is expected, such as in arithmetic operations
+// - toString is called when String(obj)
+class ArrayWrapper {
+    nums: number[]
+	constructor(nums: number[]) {
+        this.nums = nums
+        
+    }
+
+	valueOf(): number {
+        let sum = 0
+        this.nums.forEach(e=>sum+=e)
+        return sum
+    }
+
+	toString(): string {
+        let string = `[${this.nums}]`
+        // this.nums.flat(Infinity)
+        // this.nums.forEach(e=> string+=e)
+        return string
+    }
+};
+const obj1 = new ArrayWrapper([1,22]);
+console.log(obj1.toString())
+
+// Notes: 
+// When you use an array in a string context (like in a template literal), 
+// JavaScript automatically converts the array to a string by joining its elements 
+// with commas. This is equivalent to calling the array’s join method with a comma as 
+// the argument. The square brackets [] are not included in the string 
+// representation of an array.
+
+// Here’s an example:
+
+// JavaScript
+// AI-generated code. Review and use carefully. More info on FAQ.
+
+// const arr = [1, 2, 3];
+// console.log(`${arr}`);  // Output: "1,2,3"
+// console.log(arr.join(','));  // Output: "1,2,3"
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
