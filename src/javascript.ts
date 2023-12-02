c7619ada858fe7916720294e0ef5e40820782ff5
spatulatom <spatulatom@gmail.com>
2023-12-02 23:11:09 +0000

    2666. Allow One Function Call, closure

--- src/javascript.ts
+++ src/javascript.ts
@@ -164,7 +164,7 @@ var flat = function (
 
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
-        console.log(arr[i]);
+        // console.log(arr[i]);
         flatt.push(...(arr[i] as MultiDimensionalArray));
       } else {
         flatt.push(arr[i]);
@@ -306,9 +306,9 @@ function createCounter(init: number): ReturnObj {
 }
 const counter = createCounter(2);
 console.log(counter.increment());
-console.log(counter.increment());
-console.log(counter.reset());
-console.log(counter.increment());
+// console.log(counter.increment());
+// console.log(counter.reset());
+// console.log(counter.increment());
 
 /**
  * const counter = createCounter(5)
@@ -463,6 +463,70 @@ console.log(obj1.toString())
 		
 // 	};
 // }
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
+
+
+// 2666. Allow One Function Call
+
+// Given a function fn, return a new function that is identical to the original function 
+// except that it ensures fn is called at most once.
+
+// The first time the returned function is called, it should return the same result as fn.
+// Every subsequent time it is called, it should return undefined.
+ 
+
+// Example 1:
+
+// Input: fn = (a,b,c) => (a + b + c), calls = [[1,2,3],[2,3,6]]
+// Output: [{"calls":1,"value":6}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(1, 2, 3); // 6
+// onceFn(2, 3, 6); // undefined, fn was not called
+// Example 2:
+
+// Input: fn = (a,b,c) => (a * b * c), calls = [[5,7,4],[2,3,6],[4,6,8]]
+// Output: [{"calls":1,"value":140}]
+// Explanation:
+// const onceFn = once(fn);
+// onceFn(5, 7, 4); // 140
+// onceFn(2, 3, 6); // undefined, fn was not called
+// onceFn(4, 6, 8); // undefined, fn was not called
+ 
+
+// Constraints:
+
+// calls is a valid JSON array
+// 1 <= calls.length <= 10
+// 1 <= calls[i].length <= 100
+// 2 <= JSON.stringify(calls).length <= 1000
+
+
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let f = fn
+    // console.log(fn)
+	return function (...args){
+        console.log('here',args)
+        let fl =f
+        f= ()=>{}
+    return fl(...args)
+    }
+}
+let fn = (a:number,b:number,c:number) => (a + b + c)
+let onceFn = once(fn)
+
+console.log(onceFn(1,2,3))
+console.log(onceFn(2,3,6))
 
 /**
  * let fn = (a,b,c) => (a + b + c)
