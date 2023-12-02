abc5faebb27ae9b9121dcf7e6fbdcf909b6dcb01
spatulatom <spatulatom@gmail.com>
2023-12-02 17:57:15 +0000

    2723. Add Two Promises, sol

--- src/javascript.ts
+++ src/javascript.ts
@@ -162,7 +162,6 @@ var flat = function (
   function recursion(n: number) {
     if (n === 0) return arr;
 
-
     for (let i = 0; i < arr.length; i++) {
       if (Array.isArray(arr[i])) {
         console.log(arr[i]);
@@ -171,8 +170,8 @@ var flat = function (
         flatt.push(arr[i]);
       }
     }
-    arr= flatt;
-    flatt =[]
+    arr = flatt;
+    flatt = [];
 
     return recursion(n - 1);
   }
@@ -192,3 +191,54 @@ console.log(
     0
   )
 );
+
+// 2723. Add Two Promises
+// Easy
+// 182
+// 15
+// Companies
+// Given two promises promise1 and promise2, return a new promise. promise1 and
+// promise2 will both resolve with a number. The returned promise should resolve with the sum
+// of the two numbers.
+
+// Example 1:
+
+// Input:
+// promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20)),
+// promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
+// Output: 7
+// Explanation: The two input promises resolve with the values of 2 and 5 respectively.
+// The returned promise should resolve with a value of 2 + 5 = 7. The time the returned
+// promise resolves is not judged for this problem.
+// Example 2:
+
+// Input:
+// promise1 = new Promise(resolve => setTimeout(() => resolve(10), 50)),
+// promise2 = new Promise(resolve => setTimeout(() => resolve(-12), 30))
+// Output: -2
+// Explanation: The two input promises resolve with the values of 10 and -12 respectively.
+// The returned promise should resolve with a value of 10 + -12 = -2.
+
+// Constraints:
+
+// promise1 and promise2 are promises that resolve with a number
+
+type P = Promise<number>;
+
+async function addTwoPromises(promise1: P, promise2: P): P {
+  const one = await promise1;const two = await promise2
+  return new Promise((resolve)=> resolve(one+two))
+}
+
+console.log(
+  'addTwoPromises',
+  addTwoPromises(
+    new Promise((resolve) => setTimeout(() => resolve(2), 20)),
+    new Promise((resolve) => setTimeout(() => resolve(5), 60))
+  ).then(console.log)
+);
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
