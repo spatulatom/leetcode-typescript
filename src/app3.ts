ff0ee12b95eaf0660b30f48de42d3c3d0822ce01
spatulatom <spatulatom@gmail.com>
2023-12-09 11:37:45 +0000

    152. Maximum Product Subarray,set up

--- src/app3.ts
+++ src/app3.ts
@@ -93,3 +93,46 @@ var divide = function(dividend: number, divisor: number): number {
 
 
 console.log('divide', divide(2147483647, 1));
+
+
+// 152. Maximum Product Subarray
+// Medium
+// 17.7K
+// 565
+// Companies
+// Given an integer array nums, find a 
+// subarray
+//  that has the largest product, and return the product.
+
+// The test cases are generated so that the answer will fit in a 32-bit integer.
+
+ 
+
+// Example 1:
+
+// Input: nums = [2,3,-2,4]
+// Output: 6
+// Explanation: [2,3] has the largest product 6.
+// Example 2:
+
+// Input: nums = [-2,0,-1]
+// Output: 0
+// Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 2 * 104
+// -10 <= nums[i] <= 10
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// Accepted
+// 1.1M
+// Submissions
+// 3.3M
+// Acceptance Rate
+// 34.9%
+
+
+function maxProduct(nums: number[]): number {
+    
+};
