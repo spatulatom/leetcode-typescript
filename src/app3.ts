85e87ee7076bb43e95614e34f297889b0becab7f
spatulatom <spatulatom@gmail.com>
2023-12-09 18:30:43 +0000

    152. Maximum Product Subarray, brute force On^2

--- src/app3.ts
+++ src/app3.ts
@@ -33,19 +33,17 @@
 // -231 <= dividend, divisor <= 231 - 1
 // divisor != 0
 
-
 // sol 1, naive, eceeds time
 function divide1(dividend: number, divisor: number): number {
   const dividendAbs = Math.abs(dividend);
   const divisorAbs = Math.abs(divisor);
-  
-  if((dividendAbs |0) !== dividendAbs){
-    console.log('here')
-    if (dividend < 0 && divisor > 0) return dividend
-    if (dividend > 0 && divisor < 0) return dividend-1
-    if (dividend < 0 && divisor < 0) return -dividend-1
-    return dividend-1
 
+  if ((dividendAbs | 0) !== dividendAbs) {
+    console.log('here');
+    if (dividend < 0 && divisor > 0) return dividend;
+    if (dividend > 0 && divisor < 0) return dividend - 1;
+    if (dividend < 0 && divisor < 0) return -dividend - 1;
+    return dividend - 1;
   }
 
   let count = 0;
@@ -53,12 +51,12 @@ function divide1(dividend: number, divisor: number): number {
   if (divisorAbs === 1) {
     count = dividendAbs;
   } else {
-    for (let i = 0; i <= dividendAbs; i=i+divisorAbs) {
+    for (let i = 0; i <= dividendAbs; i = i + divisorAbs) {
       if (i - total === divisorAbs) {
         count++;
         total += divisorAbs;
       }
-      console.log(i,count,total);
+      console.log(i, count, total);
     }
   }
 
@@ -67,46 +65,44 @@ function divide1(dividend: number, divisor: number): number {
   return count;
 }
 
-// sol 2, 
-var divide = function(dividend: number, divisor: number): number {
-    const retIsNegative = Math.sign(divisor) !== Math.sign(dividend);
-    dividend = Math.abs(dividend)
-    divisor = Math.abs(divisor)
-    
-    let ret = 0
-    while (divisor <= dividend) {
-        let value = divisor
-        let multiple = 1
-        while (value + value <= dividend) {
-            value += value
-            multiple += multiple
-        }
-        dividend = dividend - value
-        ret += multiple
+// sol 2,
+var divide = function (dividend: number, divisor: number): number {
+  const retIsNegative = Math.sign(divisor) !== Math.sign(dividend);
+  dividend = Math.abs(dividend);
+  divisor = Math.abs(divisor);
+
+  let ret = 0;
+  while (divisor <= dividend) {
+    console.log('loop1', divisor, dividend);
+    let value = divisor;
+    let multiple = 1;
+    while (value + value <= dividend) {
+      value += value;
+      multiple += multiple;
+      console.log('loop2', divisor, dividend, value);
     }
-    
-    if (ret > ((2**31) - 1)) {
-        return retIsNegative ? -(2**31) : 2**31 - 1
-    }
-    return retIsNegative ? -ret : ret
+    dividend = dividend - value;
+    ret += multiple;
+  }
+
+  if (ret > 2 ** 31 - 1) {
+    return retIsNegative ? -(2 ** 31) : 2 ** 31 - 1;
+  }
+  return retIsNegative ? -ret : ret;
 };
 
-
-console.log('divide', divide(2147483647, 1));
-
+console.log('divide', divide(30, 3));
 
 // 152. Maximum Product Subarray
 // Medium
 // 17.7K
 // 565
 // Companies
-// Given an integer array nums, find a 
+// Given an integer array nums, find a
 // subarray
 //  that has the largest product, and return the product.
 
 // The test cases are generated so that the answer will fit in a 32-bit integer.
-
- 
 
 // Example 1:
 
@@ -118,7 +114,6 @@ console.log('divide', divide(2147483647, 1));
 // Input: nums = [-2,0,-1]
 // Output: 0
 // Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
- 
 
 // Constraints:
 
@@ -132,7 +127,33 @@ console.log('divide', divide(2147483647, 1));
 // Acceptance Rate
 // 34.9%
 
-
+// sol 1, brute force, O(n^2), space complexity O(n)
 function maxProduct(nums: number[]): number {
-    
-};
+  const allSums = new Set();
+ if(nums.length===1) return nums[0]
+ 
+  for (let i = 0; i < nums.length; i++) {
+    if(nums[i]===0)continue
+    let sum = 1
+    let length;
+    if (i === 0) {
+      length = nums.length - 2;
+    } else {
+      nums.length - 1;
+    }
+    length = nums.length - 1;
+    for (let j = i; j < nums.length; j++) {
+       
+      sum *= nums[j];
+      console.log('sum', sum, nums[j])
+      allSums.add(sum);
+    }
+  }
+  let max: number = -Infinity;
+  const sum = [...allSums].forEach((e: any) => {
+    max = Math.max(max, e);
+  });
+  return max
+}
+
+console.log('maxProduct', maxProduct([0,2]));
