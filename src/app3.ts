5c02a456d37138613101e031fc34cdb16480a0ad
spatulatom <spatulatom@gmail.com>
2023-12-10 01:24:56 +0000

    53. Maximum Subarray,  brute force O(n^2)

--- src/app3.ts
+++ src/app3.ts
@@ -234,7 +234,6 @@ function productExceptSelf(nums: number[]): number[] {
       result[i] = product;
       product *= nums[i];
     }
-      
     // Modify from end to start
     product = 1;
     for (let i = nums.length - 1; i >= 0; i -= 1) {
@@ -245,3 +244,60 @@ function productExceptSelf(nums: number[]): number[] {
     return result;
   }
 console.log('productExceptSelf',productExceptSelf( [1,2,3,4]))
+
+
+// 53. Maximum Subarray
+// Medium
+// 32.7K
+// 1.4K
+// Companies
+// Given an integer array nums, find the 
+// subarray
+//  with the largest sum, and return its sum.
+
+ 
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+// Example 2:
+
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+// Example 3:
+
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+ 
+
+// Follow up: If you have figured out the O(n) solution, try coding another 
+// solution using 
+// the divide and conquer approach, which is more subtle.
+
+// sol 1, brute force O(n^2)
+function maxSubArray(nums: number[]): number {
+    let max: number = -Infinity;
+    if (nums.length === 1) return nums[0];
+  
+    for (let i = 0; i < nums.length; i++) {
+      let sum = 0;
+      for (let j = i; j < nums.length; j++) {
+        sum += nums[j];
+        max = Math.max(max, sum);
+      }
+    }
+  
+    return max;
+    
+};
+
+console.log('maxSubArray',maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
