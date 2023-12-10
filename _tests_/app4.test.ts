6b4aab1a21ac83400b4f19afd33fb6e9783f8784
spatulatom <spatulatom@gmail.com>
2023-12-10 23:46:03 +0000

    345. Reverse Vowels of a String, Jest test 2

--- _tests_/app4.test.ts
+++ _tests_/app4.test.ts
@@ -3,3 +3,7 @@ import { reverseVowels } from "../src/app4";
 it('should return an empty string when given an empty string', () => {
     expect(reverseVowels('')).toBe('');
   });
+
+  it('should return the same string when there are no vowels in the string', () => {
+    expect(reverseVowels('bcd')).toBe('bcd');
+  });
