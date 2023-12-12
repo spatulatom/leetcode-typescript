0f6b65aacea70a5b1bcf85e36f98c613648f1f2b
spatulatom <spatulatom@gmail.com>
2023-12-12 13:01:33 +0000

    6. Zigzag Conversion, set up

--- src/app4.ts
+++ src/app4.ts
@@ -73,7 +73,118 @@ export function reverseVowels(s: string): string {
   return array.join('');
 }
 
-
-
-
 console.log('reverseVowels', reverseVowels('hello'));
+
+
+// 6. Zigzag Conversion
+// Medium
+// 7K
+// 13.8K
+// Companies
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a 
+// given number of rows like this: (you may want to display this pattern in a 
+//     fixed font for better legibility)
+
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR"
+
+// Write the code that will take a string and make this conversion given a 
+// number of rows:
+
+// string convert(string s, int numRows);
+ 
+
+// Example 1:
+
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+// Example 2:
+
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+// Example 3:
+
+// Input: s = "A", numRows = 1
+// Output: "A"
+ 
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consists of English letters (lower-case and upper-case), ',' and '.'.
+// 1 <= numRows <= 1000
+// Accepted
+// 1.2M
+// Submissions
+// 2.5M
+// Acceptance Rate
+// 46.5%
+
+
+function convert(s: string, numRows: number): string {
+    
+};
+
+console.log('convert:', convert("PAYPALISHIRING", 3))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
