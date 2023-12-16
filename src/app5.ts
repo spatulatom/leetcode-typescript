f0ef07f7efbbc3d8e0cf191a64a908f22c940dc7
spatulatom <spatulatom@gmail.com>
2023-12-16 10:52:02 +0000

    73. Set Matrix Zeroes, set up

--- src/app5.ts
+++ src/app5.ts
@@ -184,3 +184,51 @@ function exist2(board: string[][], word: string): boolean {
     return false; // Word not found in the entire board
 }
 
+
+
+// 73. Set Matrix Zeroes
+// Medium
+// 13.6K
+// 680
+// Companies
+// Given an m x n integer matrix matrix, if an element is 0, set its entire 
+// row and column to 0's.
+
+// You must do it in place.
+
+//  Example 1:
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+// Example 2:
+
+
+// Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+// Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+ 
+
+// Constraints:
+
+// m == matrix.length
+// n == matrix[0].length
+// 1 <= m, n <= 200
+// -231 <= matrix[i][j] <= 231 - 1
+ 
+
+// Follow up:
+
+// A straightforward solution using O(mn) space is probably a bad idea.
+// A simple improvement uses O(m + n) space, but still not the best solution.
+// Could you devise a constant space solution?
+// Accepted
+// 1.2M
+// Submissions
+// 2.3M
+
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+ function setZeroes(matrix: number[][]): void {
+    
+ };
+
+ console.log('setZeroes', setZeroes())
