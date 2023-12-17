5dc2617468672d5c8379cd5f1833b394450c9c39
spatulatom <spatulatom@gmail.com>
2023-12-17 11:18:28 +0000

    Test suites, formatting

--- _tests_/app6.test.ts
+++ _tests_/app6.test.ts
@@ -1,27 +1,48 @@
-
 // 56. Merge Intervals
-import { merge56 } from "../src/app6";
+import { merge56 } from '../src/app6';
 
 it('should merge overlapping intervals and return non-overlapping intervals', () => {
-    const intervals = [[1,3],[2,6],[8,10],[15,18]];
-    const result = merge56(intervals);
-    expect(result).toEqual([[1,6],[8,10],[15,18]]);
-  });
+  const intervals = [
+    [1, 3],
+    [2, 6],
+    [8, 10],
+    [15, 18],
+  ];
+  const result = merge56(intervals);
+  expect(result).toEqual([
+    [1, 6],
+    [8, 10],
+    [15, 18],
+  ]);
+});
 
-  it('should merge intervals with same start and end times', () => {
-    const intervals = [[1,4],[1,4]];
-    const result = merge56(intervals);
-    expect(result).toEqual([[1,4]]);
-  });
+it('should merge intervals with same start and end times', () => {
+  const intervals = [
+    [1, 4],
+    [1, 4],
+  ];
+  const result = merge56(intervals);
+  expect(result).toEqual([[1, 4]]);
+});
 
-  it('should return same array for single interval input', () => {
-    const intervals = [[1,4]];
-    const result = merge56(intervals);
-    expect(result).toEqual([[1,4]]);
-  });
+it('should return same array for single interval input', () => {
+  const intervals = [[1, 4]];
+  const result = merge56(intervals);
+  expect(result).toEqual([[1, 4]]);
+});
 
-  it('should return same array for non-overlapping intervals', () => {
-    const intervals = [[1,3],[4,6],[8,10],[15,18]];
-    const result = merge56(intervals);
-    expect(result).toEqual([[1,3],[4,6],[8,10],[15,18]]);
-  });
+it('should return same array for non-overlapping intervals', () => {
+  const intervals = [
+    [1, 3],
+    [4, 6],
+    [8, 10],
+    [15, 18],
+  ];
+  const result = merge56(intervals);
+  expect(result).toEqual([
+    [1, 3],
+    [4, 6],
+    [8, 10],
+    [15, 18],
+  ]);
+});
