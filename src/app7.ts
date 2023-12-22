9bbdd82fd61a5c2f60efaeef31968fef7c0bbec2
spatulatom <spatulatom@gmail.com>
2023-12-22 23:51:22 +0000

    322. Coin Change, description

--- src/app7.ts
+++ src/app7.ts
@@ -96,4 +96,42 @@ console.log('maxSubArray', maxSubArray00([-1, -2]));
 // to use can depend on the specific requirements of the problem and the characteristics
 // of the input data. It's always beneficial to understand multiple approaches to a
 // problem, as it can give you a deeper understanding of the problem and help you become
-// a better problem solver. Keep up the great work! 😊
+// a better problem solver. Keep up the great work!
+
+
+
+322. Coin Change
+Medium
+18.1K
+421
+Companies
+You are given an integer array coins representing coins of different denominations 
+and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that 
+amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+ 
+
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
