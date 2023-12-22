f24f62fde1c814d99a72cbeb11b65f20b548672c
spatulatom <spatulatom@gmail.com>
2023-12-22 23:53:38 +0000

    322. Coin Change, brute force set up

--- src/app7.ts
+++ src/app7.ts
@@ -100,42 +100,47 @@ console.log('maxSubArray', maxSubArray00([-1, -2]));
 
 
 
-322. Coin Change
-Medium
-18.1K
-421
-Companies
-You are given an integer array coins representing coins of different denominations 
-and an integer amount representing a total amount of money.
+// 322. Coin Change
+// Medium
+// 18.1K
+// 421
+// Companies
+// You are given an integer array coins representing coins of different denominations 
+// and an integer amount representing a total amount of money.
 
-Return the fewest number of coins that you need to make up that amount. If that 
-amount of money cannot be made up by any combination of the coins, return -1.
+// Return the fewest number of coins that you need to make up that amount. If that 
+// amount of money cannot be made up by any combination of the coins, return -1.
 
-You may assume that you have an infinite number of each kind of coin.
+// You may assume that you have an infinite number of each kind of coin.
 
  
 
-Example 1:
+// Example 1:
 
-Input: coins = [1,2,5], amount = 11
-Output: 3
-Explanation: 11 = 5 + 5 + 1
-Example 2:
+// Input: coins = [1,2,5], amount = 11
+// Output: 3
+// Explanation: 11 = 5 + 5 + 1
+// Example 2:
 
-Input: coins = [2], amount = 3
-Output: -1
-Example 3:
+// Input: coins = [2], amount = 3
+// Output: -1
+// Example 3:
 
-Input: coins = [1], amount = 0
-Output: 0
+// Input: coins = [1], amount = 0
+// Output: 0
  
 
-Constraints:
+// Constraints:
 
-1 <= coins.length <= 12
-1 <= coins[i] <= 231 - 1
-0 <= amount <= 104
+// 1 <= coins.length <= 12
+// 1 <= coins[i] <= 231 - 1
+// 0 <= amount <= 104
 
+// sol 1, brute force 
 function coinChange(coins: number[], amount: number): number {
-    
+    for(let i=0;i<coins.length;i++){
+        
+    }
 };
+
+console.log('coinChange', coinChange([1,2,5],11))
