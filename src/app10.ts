e3a8a58eb68ca7c6dce8b4698002e30d23ae95fc
spatulatom <spatulatom@gmail.com>
2024-01-13 12:28:10 +0000

    819, 47 / 48 testcases passed

--- src/app10.ts
+++ src/app10.ts
@@ -3,32 +3,29 @@
 // 1.6K
 // 3K
 // Companies
-// Given a string paragraph and a string array of the banned words banned, 
-// return the most frequent word that is not banned. It is guaranteed there 
+// Given a string paragraph and a string array of the banned words banned,
+// return the most frequent word that is not banned. It is guaranteed there
 // is at least one word that is not banned, and that the answer is unique.
 
-// The words in paragraph are case-insensitive and the answer should be returned 
+// The words in paragraph are case-insensitive and the answer should be returned
 // in lowercase.
-
- 
 
 // Example 1:
 
-// Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", 
+// Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.",
 // banned = ["hit"]
 // Output: "ball"
-// Explanation: 
+// Explanation:
 // "hit" occurs 3 times, but it is a banned word.
 // "ball" occurs twice (and no other word does), so it is the most frequent
-//  non-banned word in the paragraph. 
+//  non-banned word in the paragraph.
 // Note that words in the paragraph are not case sensitive,
-// that punctuation is ignored (even if adjacent to words, such as "ball,"), 
+// that punctuation is ignored (even if adjacent to words, such as "ball,"),
 // and that "hit" isn't the answer even though it occurs more because it is banned.
 // Example 2:
 
 // Input: paragraph = "a.", banned = []
 // Output: "a"
- 
 
 // Constraints:
 
@@ -39,5 +36,38 @@
 // banned[i] consists of only lowercase English letters.
 
 function mostCommonWord(paragraph: string, banned: string[]): string {
-    
-};
+  const arr = paragraph.split(' ')
+  const symbols = "!?',;.";
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < symbols.length; j++) {
+    //   console.log('here', arr[i][arr[i].length-1],)
+      if (arr[i][arr[i].length - 1] === symbols[j]) {
+        // console.log('here', arr[i][arr[i].length - 1]);
+        const word = arr[i].split('');
+        word.pop();
+        arr[i] = word.join('');
+      }
+    }
+    arr[i] = arr[i].toLocaleLowerCase();
+  }
+  const hash: { [key: string]: number } = {};
+  for (let i = 0; i < arr.length; i++) {
+    hash[arr[i]] = (hash[arr[i]] ?? 0) + 1;
+  }
+
+if(banned[0]){
+    banned.forEach(e=>{
+        delete hash[e.toLocaleLowerCase()]
+    })
+   
+}
+// return hash
+  return Object.entries(hash).sort((a, b) => {
+    return b[1] - a[1];
+  })[0][0];
+}
+
+console.log(
+  'mostCommonWord',
+  mostCommonWord("a, a, a, a, b,b,b,c, c", ["a"])
+);
