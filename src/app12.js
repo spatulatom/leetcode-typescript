eb29f3b2c47babdf5b7a752bff4e31afb28aca45
spatulatom <spatulatom@gmail.com>
2024-03-26 10:33:40 +0000

    vite server auditing

---
+++ src/app12.js
@@ -0,0 +1,24 @@
+// let obj: { property: number }= {property:6} // Error: Type 'null' is not assignable to type '{ property: number; }'
+// console.log(obj.property);
+
+// let userProfile: string | null=null;
+// console.log(userProfile);
+
+let sth;
+sth = null;
+sth = 6;
+sth= 'hello'
+
+console.log(sth)
+
+
+let user = {
+    name: 'John Doe',
+    // address: {
+    //   street: '123 Main St',
+    //   city: 'Anytown',
+    //   country: 'USA'
+    // }
+  };
+  
+  console.log(user.address?.city)
