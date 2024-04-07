f69c2c1ef945740f64df47558c9aeeca4e55e217
spatulatom <spatulatom@gmail.com>
2024-04-07 12:32:12 +0100

    229. Majority Element II, more Jest library tests

--- _tests_/app8.test.ts
+++ _tests_/app8.test.ts
@@ -11,3 +11,9 @@ it('should return an array with the majority element when it appears more than n
     const result = majorityElement1(nums);
     expect(result).toEqual([]);
   });
+
+  it('should return an array with one element when the input array has only one element and it appears more than n/3 times', () => {
+    const nums = [1];
+    const result = majorityElement1(nums);
+    expect(result).toEqual([1]);
+  });
