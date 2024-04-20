ac714a68ee0d3dd0ea08239a07da0d9f5891dcf5
spatulatom <spatulatom@gmail.com>
2024-04-20 15:22:56 +0100

    53. Maximum Subarray, more Jest tests

--- _tests_/app7.test.ts
+++ _tests_/app7.test.ts
@@ -6,3 +6,9 @@ it('should return the maximum sum of a contiguous subarray when the array has po
     const result = maxSubArray00(nums);
     expect(result).toBe(15);
   });
+
+  it('should return the correct maximum sum for an array with both positive and negative integers', () => {
+    const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+    const result = maxSubArray00(nums);
+    expect(result).toBe(6);
+  });
