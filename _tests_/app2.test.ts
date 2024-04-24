4b0312ee13ea5b28ce18406972fd44ac1892da9a
spatulatom <spatulatom@gmail.com>
2024-04-24 12:23:58 +0100

    more Jest linrary tests

--- _tests_/app2.test.ts
+++ _tests_/app2.test.ts
@@ -8,3 +8,7 @@ it('should return 1 when given a string with length 1', () => {
 it('should return 0 when given an empty string', () => {
   expect(longestPalindrome('')).toBe(0);
 });
+
+it('should return the length of the string when all characters are the same', () => {
+    expect(longestPalindrome('aaaaaa')).toBe(6);
+  });
