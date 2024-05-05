878c6fa83262a91e941919b6afdccc9357f41bb8
spatulatom <spatulatom@gmail.com>
2024-05-05 15:51:39 +0100

    29. Divide Two Integers, more Jest tests

--- _tests_/app3.test.ts
+++ _tests_/app3.test.ts
@@ -1,12 +1,29 @@
 // 29. Divide Two Integers
-import { divide1 } from "../src/app3";
+import { divide1 } from '../src/app3';
 
 it('should return the correct quotient when dividend and divisor are positive', () => {
-    expect(divide1(10, 3)).toBe(3);
-    expect(divide1(15, 5)).toBe(3);
-    expect(divide1(20, 4)).toBe(5);
-  });
+  expect(divide1(10, 3)).toBe(3);
+  expect(divide1(15, 5)).toBe(3);
+  expect(divide1(20, 4)).toBe(5);
+});
 
-  it('should return 2147483647 when dividend is -2147483648 and divisor is -1', () => {
-    expect(divide1(-2147483648, -1)).toBe(2147483647);
-  })
+it('should return 2147483647 when dividend is -2147483648 and divisor is -1', () => {
+  expect(divide1(-2147483648, -1)).toBe(2147483647);
+});
+
+it('should return the correct quotient when dividend is positive and divisor is negative', () => {
+  expect(divide1(10, -3)).toBe(-3);
+  expect(divide1(15, -5)).toBe(-3);
+  expect(divide1(20, -4)).toBe(-5);
+});
+
+it('should return the correct quotient when dividend is negative and divisor is positive', () => {
+  expect(divide1(-10, 3)).toBe(-3);
+  expect(divide1(-15, 5)).toBe(-3);
+  expect(divide1(-20, 4)).toBe(-5);
+});
+it('should return the correct quotient when dividend and divisor are negative', () => {
+  expect(divide1(-10, -3)).toBe(3);
+  expect(divide1(-15, -5)).toBe(3);
+  expect(divide1(-20, -4)).toBe(5);
+});
