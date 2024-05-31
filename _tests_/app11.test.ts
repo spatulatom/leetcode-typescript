e52f88cbd6ae83b535da0e3d42b7f937916b99ed
spatulatom <spatulatom@gmail.com>
2024-05-31 10:14:54 +0100

    // 139. Word Break, more Jest library tests

--- _tests_/app11.test.ts
+++ _tests_/app11.test.ts
@@ -12,3 +12,16 @@ it('should return false when the string is a single character not in the diction
   const wordDict = ['b'];
   expect(wordBreak(s, wordDict)).toBe(false);
 });
+
+it('should return false when the string cannot be segmented using dictionary words', () => {
+    const s = "catsandog";
+    const wordDict = ["cats", "dog", "sand", "and", "cat"];
+    expect(wordBreak(s, wordDict)).toBe(false);
+  });
+
+
+  it('should return true when the string can be segmented using a dictionary with words of varying lengths', () => {
+    const s = "applepenapple";
+    const wordDict = ["apple", "pen"];
+    expect(wordBreak(s, wordDict)).toBe(true);
+  });
